f8576c5c74f17a201ecea6aec6dc6e013ff042fd
H0onnn <dukei2017@naver.com>
2023-07-16 18:36:20 +0900

    Comment : 사이드바 아이템 컴포넌트 주석 추가

--- components/SidebarItem.tsx
+++ components/SidebarItem.tsx
@@ -1,14 +1,13 @@
 import Link from "next/link";
 import React from "react";
-
 import { IconType } from "react-icons";
 import { twMerge } from "tailwind-merge";
 
 interface SidebarItemProps {
-  icon: IconType;
-  label: string;
-  active: boolean;
-  href: string;
+  icon: IconType; // 아이콘
+  label: string; // 레이블 (표시될 텍스트)
+  active: boolean; // 활성화 여부 (현재 선택된 아이템인지 표시)
+  href: string; // 링크
 }
 
 const SidebarItem: React.FC<SidebarItemProps> = ({
@@ -35,7 +34,7 @@ const SidebarItem: React.FC<SidebarItemProps> = ({
     transition
     text-neutral-400
     py-1`,
-        active && "text-white"
+        active && "text-white" // 활성화 상태에 따라 text-white 클래스 추가
       )}
     >
       <Icon size={26} />
